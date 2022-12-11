a9c7462e2611df0a71ac442d8d60dd98c369e659
Kashaf Aiman <aimankashaf33@gmail.com>
2022-12-11 21:30:10 +0530

    final commit

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className='gpt3__footer section__padding'>
       <div className="gpt3__footer-heading">
-        <h1 className="gradeint_text"> Do you want to step in to the future before others </h1>
+        <h1 className="gradient_text"> Do you want to step in to the future before others </h1>
       </div>
       <div className="gpt3__footer-btn">
         <p>Request Early Access</p>
@@ -18,25 +18,25 @@ const Footer = () => {
         </div>
         <div className="gpt__footer-links_div">
           <h4>Links</h4>
-          <h4>Overons</h4>
-          <h4>Social Media</h4>
-          <h4>Counters</h4>
-          <h4>Contact</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
         </div>
         <div className="gpt__footer-links_div">
           <h4>Company</h4>
-          <h4>Terms & Conditions</h4>
-          <h4>Privacy Policy</h4>
-          <h4>Contact</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
         </div>
         <div className="gpt__footer-links_div">
           <h4>Get in touch</h4>
-          <h4>Crechterwoord K12 182 DK Alknjkcb</h4>
-          <h4>085-132567</h4>
-          <h4>info@payme.net</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085-132567</p>
+          <p>info@payme.net</p>
         </div>
       </div>
-      <div className="gpt3__footer-copywrite">
+      <div className="gpt3__footer-copyright">
         <p>© 2021 GPT-3. All rights reserved.</p>
       </div>
     </div>
